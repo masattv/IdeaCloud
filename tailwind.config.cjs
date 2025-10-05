78921f707149fb2513b1ca07111000b7d8b18800
masattv <k3absorb430@gmail.com>
2025-10-06 08:54:41 +0900

    Adopt modern sans-serif typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,2 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
-module.exports = { content: ['./index.html', './src/**/*.{ts,tsx}'], theme: { extend: {} }, plugins: [] }
+module.exports = {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', '"Noto Sans JP"', ...defaultTheme.fontFamily.sans],
+        heading: ['"Plus Jakarta Sans"', '"Noto Sans JP"', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  plugins: []
+}
